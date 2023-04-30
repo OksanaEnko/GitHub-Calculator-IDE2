20dfcd717c328cb7d5a0ab3a95cbe6b60d3b980f
OksanaEnko <oksanaenko4@gmail.com>
2023-04-30 23:16:54 +0200

    App subtraction and division

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,10 +3,10 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My app for Users!");
-        System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
+        System.out.println("My git the best");
+
+        int a = 10;
+        int b = 5;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
@@ -14,12 +14,12 @@ public class Main {
     }
 
     private static int add(int a, int b) {
-
-        return a + b;
+        return a - b;
     }
 
     private static int multiply(int a, int b) {
-        return a * b;
+
+        return a / b;
     }
 }
 
