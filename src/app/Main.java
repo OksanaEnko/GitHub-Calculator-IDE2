f9aea707c5b0887b2c22c64fb20a50446f81748e
OksanaEnko <oksanaenko4@gmail.com>
2023-04-30 22:25:14 +0200

    Change app info: short text

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("Super app!");
 
         int a = 8;
         int b = 2;
@@ -12,7 +12,7 @@ public class Main {
     }
 
     private static int add(int a, int b) {
+
         return a + b;
     }
 }
-
